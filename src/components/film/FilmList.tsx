f3230f41605b0829160e852aba13868bc1e5ecf0
NarciSource <jwch11@gmail.com>
2024-07-12 23:19:36 +0900

    feat: 영화 목록에 Waypoint 무한스크롤 기능 추가

--- src/components/film/FilmList.tsx
+++ src/components/film/FilmList.tsx
@@ -1,10 +1,11 @@
 import { Box, SimpleGrid, Skeleton } from '@chakra-ui/react';
 import { useFilmsQuery } from '../../generated/graphql';
 import FilmCard from './FilmCard';
+import { Waypoint } from 'react-waypoint';
 
 export default function FilmList(): JSX.Element {
     const LIMIT = 6;
-    const { data, loading, error } = useFilmsQuery({
+    const { data, loading, error, fetchMore } = useFilmsQuery({
         variables: { limit: LIMIT, cursor: 1 },
     });
 
@@ -15,9 +16,18 @@ export default function FilmList(): JSX.Element {
             {loading && new Array(6).fill(0).map((x, i) => <Skeleton key={i} height="400px" />)}
             {!loading &&
                 data &&
-                data.films.films.map((film) => (
+                data.films.films.map((film, i) => (
                     <Box key={film.id}>
-                        {film.id}
+                        {data.films.cursor && i === data.films.films.length - LIMIT / 2 && (
+                            /* 4번째 데이터에서 Waypoint 엮임 */
+                            <Waypoint
+                                onEnter={() => {
+                                    fetchMore({
+                                        variables: { limit: LIMIT, cursor: data.films.cursor },
+                                    });
+                                }}
+                            />
+                        )}
                         <FilmCard film={film} />
                     </Box>
                 ))}
