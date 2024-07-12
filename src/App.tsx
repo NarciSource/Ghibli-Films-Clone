b1426f787df37410ba2df6532319daf1feadc79f
NarciSource <jwch11@gmail.com>
2024-07-12 23:48:41 +0900

    feat: Router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { ApolloProvider } from '@apollo/client';
-import { ChakraProvider, Box, Text, theme } from '@chakra-ui/react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 import * as React from 'react';
-import FilmList from './components/film/FilmList';
 import { createApolloClient } from './apollo/createApolloClient';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
 
 const apolloClient = createApolloClient();
 
 export const App: React.FC = () => (
     <ApolloProvider client={apolloClient}>
         <ChakraProvider theme={theme}>
-            <Box>
-                <Text>Ghibli GraphQL</Text>
-                <FilmList></FilmList>
-            </Box>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                </Routes>
+            </BrowserRouter>
         </ChakraProvider>
     </ApolloProvider>
 );
