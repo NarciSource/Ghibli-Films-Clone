f06be0e1234bec2b8938b79138b7cbe5ed78d12d
NarciSource <jwch11@gmail.com>
2024-07-12 22:48:25 +0900

    feat: 커서 기반 페이지네이션

--- src/resolvers/Film.ts
+++ src/resolvers/Film.ts
@@ -1,13 +1,30 @@
-import { Query, Resolver, FieldResolver, Root } from 'type-graphql';
+import { Query, Resolver, FieldResolver, Root, Int, Arg } from 'type-graphql';
 import ghibliData from '../data/ghibli';
 import { Film } from '../entities/Film';
 import { Director } from '../entities/Director';
+import { PaginatedFilms } from '../entities/PaginatedFilm';
 
 @Resolver(Film) // 인자: 오브젝트타입
 export class FilmResolver {
-    @Query(() => [Film])
-    films(): Film[] {
-        return ghibliData.films;
+    @Query(() => PaginatedFilms)
+    films(
+        @Arg('limit', () => Int, { nullable: true, defaultValue: 6 })
+        limit?: number,
+        @Arg('cursor', () => Int, { nullable: true, defaultValue: 1 })
+        cursor?: Film['id'],
+    ): PaginatedFilms {
+        const cursorDataIndex = ghibliData.films.findIndex((f) => f.id === cursor);
+
+        if (cursorDataIndex !== -1) {
+            const result = ghibliData.films.slice(cursorDataIndex, cursorDataIndex + limit);
+            const nextCursor = result[result.length - 1].id + 1;
+
+            return {
+                films: result,
+                cursor: nextCursor,
+            };
+        }
+        return { films: [] };
     }
 
     @FieldResolver(() => Director)
@@ -18,5 +35,8 @@ export class FilmResolver {
 
 // --SDL--
 // type Query {
-//     films: [Film]
+//     films: {
+//         cursor: Int
+//         films: [Film]
+//     }
 // }
