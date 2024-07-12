ecb8bf878687b96d25062a1d703b1dc35cad85e4
NarciSource <jwch11@gmail.com>
2024-07-12 23:56:17 +0900

    feat: 공통 Layout 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,12 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
 import FilmList from '../components/film/FilmList';
+import CommonLayout from '../components/CommonLayout';
 
 export default function Main(): React.ReactElement {
     return (
-        <Box>
+        <CommonLayout>
             <Heading size="lg">최고의 장면을 찾아보세요</Heading>
             <FilmList />
-        </Box>
+        </CommonLayout>
     );
 }
