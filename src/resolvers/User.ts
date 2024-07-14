d886e9f838286536202a4158eb48c54adbfd7653
NarciSource <jwch11@gmail.com>
2024-07-14 18:59:52 +0900

    feat: 유저 로그인 뮤테이션 리졸버 추가 (login)

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,5 +1,5 @@
 import argon2 from 'argon2';
-import { InputType, Arg, Field, Mutation, Resolver } from 'type-graphql';
+import { InputType, Arg, Field, Mutation, Resolver, ObjectType } from 'type-graphql';
 import { IsEmail, IsString } from 'class-validator'; // 입력 필드 유효성 검사
 import { User } from '../entities/User';
 
@@ -16,6 +16,38 @@ export class SignUpInput {
     @Field()
     @IsString()
     password: string;
+}
+
+@InputType()
+export class LoginInput {
+    @Field()
+    @IsString()
+    emailOrUsername: string;
+
+    @Field()
+    @IsString()
+    password: string;
+}
+
+@ObjectType({ description: '필드 에러 타입' })
+export class FieldError {
+    @Field()
+    field: string;
+
+    @Field()
+    message: string;
+}
+
+@ObjectType({ description: '로그인 반환 데이터' })
+export class LoginResponse {
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[];
+
+    @Field(() => User, { nullable: true })
+    user?: User;
+
+    @Field({ nullable: true })
+    accessToken?: string;
 }
 
 @Resolver(User)
@@ -36,5 +68,28 @@ export default class UserResolver {
 
         await newUser.save();
         return newUser;
+    }
+
+    @Mutation(() => LoginResponse)
+    async login(
+        @Arg('loginInput')
+        loginInput: LoginInput,
+    ): Promise<LoginResponse> {
+        const { emailOrUsername, password } = loginInput;
+
+        const user = await User.findOne({
+            where: [{ email: emailOrUsername }, { username: emailOrUsername }],
+        });
+        const isValid = await argon2.verify(user?.password, password);
+
+        let response: LoginResponse;
+        if (isValid) {
+            response = { user };
+        } else if (user) {
+            response = { errors: [{ field: 'password', message: '비밀번호가 틀렸습니다.' }] };
+        } else {
+            response = { errors: [{ field: 'emailOrUsername', message: '해당하는 유저가 없습니다.' }] };
+        }
+        return response;
     }
 }
