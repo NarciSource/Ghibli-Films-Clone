b02801f98c5f309f47277c6913d4d170541fb45f
NarciSource <jwch11@gmail.com>
2024-07-14 14:40:35 +0900

    feat: 회원가입 폼에서 폼 제출

--- src/components/auth/SignUpRealForm.tsx
+++ src/components/auth/SignUpRealForm.tsx
@@ -1,8 +1,29 @@
-import { Stack, FormControl, FormLabel, Input, Divider, Button } from '@chakra-ui/react';
+import { Stack, FormControl, FormLabel, Input, Divider, Button, useToast } from '@chakra-ui/react';
+import { SignUpInput, useSignUpMutation } from '../../generated/graphql';
 
 export default function SignUpRealForm(): React.ReactElement {
+    const [signUp, { loading }] = useSignUpMutation();
+    const toast = useToast();
+
+    const onSubmit = (event: any) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        const signUpInput: SignUpInput = {
+            email: formData.get('email') as string,
+            username: formData.get('username') as string,
+            password: formData.get('password') as string,
+        };
+
+        signUp({ variables: { signUpInput } }).then((res) => {
+            if (res.data?.signUp) {
+                toast({ title: '회원가입을 환영합니다', status: 'success' });
+            }
+        });
+    };
+
     return (
-        <Stack as="form">
+        <Stack as="form" onSubmit={onSubmit}>
             <FormControl>
                 <FormLabel>이메일</FormLabel>
                 <Input type="email" name="email" placeholder="example@example.com" />
@@ -10,7 +31,7 @@ export default function SignUpRealForm(): React.ReactElement {
 
             <FormControl>
                 <FormLabel>아이디</FormLabel>
-                <Input type="text" name="id" placeholder="example" />
+                <Input type="text" name="username" placeholder="example" />
             </FormControl>
 
             <FormControl>
@@ -19,7 +40,9 @@ export default function SignUpRealForm(): React.ReactElement {
             </FormControl>
 
             <Divider />
-            <Button type="submit">계정 생성</Button>
+            <Button type="submit" isLoading={loading}>
+                계정 생성
+            </Button>
         </Stack>
     );
 }
