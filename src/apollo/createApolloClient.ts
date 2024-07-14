53db08abb91516d7c727e49fedf3950fd53cdf5c
NarciSource <jwch11@gmail.com>
2024-07-14 22:11:57 +0900

    feat: 토큰 미들웨어 추가 (authLink)

--- src/apollo/createApolloClient.ts
+++ src/apollo/createApolloClient.ts
@@ -1,8 +1,11 @@
-import { ApolloClient, NormalizedCacheObject } from '@apollo/client';
+import { ApolloClient, from, HttpLink, NormalizedCacheObject } from '@apollo/client';
 import { createApolloCache } from './createApolloCache';
+import authLink from './middleware/authLink';
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql' });
 
 export const createApolloClient = (): ApolloClient<NormalizedCacheObject> =>
     new ApolloClient({
-        uri: 'http://localhost:4000/graphql',
+        link: from([authLink, httpLink]),
         cache: createApolloCache(),
     });
