07e6f81cccb82a64a7cc4553e8ed3843f7ee188e
NarciSource <jwch11@gmail.com>
2024-08-15 18:48:59 +0900

    feat: 명장면 좋아요 데이터 주입

--- src/components/film-cut/FilmCutDetail.tsx
+++ src/components/film-cut/FilmCutDetail.tsx
@@ -1,19 +1,38 @@
-import { AspectRatio, Box, Button, Flex, Heading, HStack, Image } from '@chakra-ui/react';
-import { CutQuery } from '../../generated/graphql';
+import { AspectRatio, Box, Button, Flex, Heading, HStack, Image, Text, useColorModeValue } from '@chakra-ui/react';
+import { CutQuery, useVoteMutation } from '../../generated/graphql';
 import { FaHeart } from 'react-icons/fa';
 
-export default function FilmCutDetail({ cut }: { cut: CutQuery['cut'] }): React.ReactElement {
+type FilmCutDetailProps = Exclude<CutQuery['cut'], null | undefined>;
+
+export default function FilmCutDetail({
+    id: cutId,
+    src: cutImg,
+    isVoted = false,
+    votesCount = 0,
+}: FilmCutDetailProps): React.ReactElement {
+    const votedButtonColor = useColorModeValue('gray.500', 'gray.400');
+
+    const [vote, { loading: voteLoading }] = useVoteMutation({ variables: { cutId } });
+
     return (
         <Box>
             <AspectRatio ratio={16 / 9}>
-                <Image src={cut?.src} objectFit="cover" />
+                <Image src={cutImg} objectFit="cover" />
             </AspectRatio>
 
             <Box py={4}>
                 <Flex justify="space-between" alignItems="center">
-                    <Heading size="sm">{cut?.id}번째 사진</Heading>
+                    <Heading size="sm">{cutId}번째 사진</Heading>
                     <HStack spacing={1} alignItems="center">
-                        <Button aria-label="like-this-cut-button" leftIcon={<FaHeart />} />
+                        <Button
+                            color={isVoted ? 'pink.400' : votedButtonColor}
+                            aria-label="like-this-cut-button"
+                            leftIcon={<FaHeart />}
+                            isLoading={voteLoading}
+                            onClick={() => vote()}
+                        >
+                            <Text>{votesCount}</Text>
+                        </Button>
                         <Button colorScheme="teal">감상 남기기</Button>
                     </HStack>
                 </Flex>
