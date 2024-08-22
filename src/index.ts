fd9b3e670bca64169e158273d0621e7eacee8a42
NarciSource <jwch11@gmail.com>
2024-08-22 21:35:34 +0900

    feat: healthcheck 추가

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import http from 'http';
-import 'reflect-metadata';
-import { createDB } from './db/db-client';
-import createApolloServer from './apollo/createApolloServer';
-import cookieParser from 'cookie-parser';
+import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import "reflect-metadata";
+import { createDB } from "./db/db-client";
+import createApolloServer from "./apollo/createApolloServer";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -12,16 +12,20 @@ async function main() {
     await createDB();
     const app = express();
     app.use((req, res, next) => {
-        res.setHeader('Referrer-Policy', 'no-referrer');
+        res.setHeader("Referrer-Policy", "no-referrer");
         next();
     }, cookieParser()); // cookie-parser 미들웨어 추가
+
+    app.get("/", (req, res) => {
+        res.status(200).send(); // for healthcheck
+    });
 
     const apolloServer = await createApolloServer();
     await apolloServer.start();
     apolloServer.applyMiddleware({
         app,
         cors: {
-            origin: [process.env.DOMAIN, 'https://studio.apollographql.com'],
+            origin: [process.env.DOMAIN, "https://studio.apollographql.com"],
             credentials: true,
         },
     });
@@ -29,7 +33,7 @@ async function main() {
     const httpServer = http.createServer(app);
 
     httpServer.listen(process.env.PORT || 4000, () => {
-        if (process.env.NODE_ENV !== 'production') {
+        if (process.env.NODE_ENV !== "production") {
             console.log(`
                 server started on => http://localhost:4000
                 graphql playground => http://localhost:4000/graphql`);
